8036bb13b07992a3b58c07c84cc7ed05d6bdcf69
dahhwe <68936213+dahhwe@users.noreply.github.com>
2021-12-29 15:35:47 +0700

    Update is_cbs.py

--- is_cbs.py
+++ is_cbs.py
@@ -1,4 +1,10 @@
 def is_cbs(string_to_check: str) -> int:
+    """
+    Функция проверяет, является ли полученная строка ПСП
+    :param string_to_check: Полученная строка
+    :return:string_to_check При заключении, что полученная строка ПСП,
+     функция возвращает 1, в противном случае 0
+    """
     brackets = ['«»', '<>', '()', '{}', '[]']
     while any(x in string_to_check for x in brackets):
         for bracket_parts in brackets:
